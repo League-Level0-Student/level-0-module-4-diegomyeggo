04954c5df922cdad662da93fb7fb06f7a0ac5e0d
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-10-17 18:32:34 -0700

    Test Score, Change Calculator

--- src/doubles_and_booleans/ChangeCalculator.java
+++ src/doubles_and_booleans/ChangeCalculator.java
@@ -15,17 +15,25 @@ public class ChangeCalculator {
 	public static void main(String[] args) {
 
 		// Ask the user how many nickels they have
-
+  String nickle = JOptionPane.showInputDialog("How many nickles do you have?");
 		// Convert their answer to an int using Integer.parseInt()
+ int Nick = Integer.parseInt(nickle);
 
 		// Ask the user how many dimes they have, and convert their answer
+String dime =JOptionPane.showInputDialog("How many dimes do you have?");
+int dime2 = Integer.parseInt(dime);
 
 		// Ask the user how many quarters they have, and convert their answer
+String quarter =JOptionPane.showInputDialog("How many quarters do you have?");
+int quart =Integer.parseInt(quarter);
+
+double val = (Nick/20.00 + dime2/10.00 + quart/4.00);
 
 		// Calculate how much money the user has and save it in a double variable 
 
-		// Tell the user how much money they have
 
+// Tell the user how much money they have
+JOptionPane.showMessageDialog(null, val);
 	}
 }
 
---
+++ src/doubles_and_booleans/TestScores.java
@@ -0,0 +1,14 @@
+package doubles_and_booleans;
+
+import javax.swing.JOptionPane;
+
+public class TestScores {
+	public static void main(String[] args) {
+		String Score = JOptionPane.showInputDialog("What was your test score?");
+		int score2 = Integer.parseInt(Score);
+		
+		if (condition) {
+			
+		}
+	}
+}
