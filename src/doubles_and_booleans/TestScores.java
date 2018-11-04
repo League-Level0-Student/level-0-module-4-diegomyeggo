498b3d3c8ef9714ee60e70b6de492d52b879b86c
TheMasterOfIron <league@worthymini-5.attlocal.net>
2018-11-04 13:02:46 -0800

    SleepyHead,TestScore,StringMethods1

--- src/doubles_and_booleans/TestScores.java
+++ src/doubles_and_booleans/TestScores.java
@@ -4,11 +4,17 @@ import javax.swing.JOptionPane;
 
 public class TestScores {
 	public static void main(String[] args) {
-		String Score = JOptionPane.showInputDialog("What was your test score?");
-		int score2 = Integer.parseInt(Score);
 		
-		if (condition) {
-			
+		String Score = JOptionPane.showInputDialog("What was your test score?");
+		
+		
+		double score2 = Double.parseDouble(Score);
+		
+		if (score2>80.00) {
+			JOptionPane.showMessageDialog(null, "wow you really studied hard!");
+		}
+		else if (score2<79.99) {
+			JOptionPane.showMessageDialog(null, "You could have used some more studying");
 		}
 	}
 }
